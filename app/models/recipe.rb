b717a25ea1bad5678bb9e749db43941a6b30c50f
Rémon Wobbema <remonwobbema@MacBook-Air-van-Remon.local>
2023-12-01 10:51:16 +0100

    Setup db structure

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  belongs_to :author
+  belongs_to :category
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
--- db/migrate/20231201085553_create_recipes.rb
+++ db/migrate/20231201085553_create_recipes.rb
@@ -1,7 +1,11 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-
+      t.string :title
+      t.integer :cook_time
+      t.integer :prep_time
+      t.numeric :ratings, precision: 3, scale: 2
+      t.string :image_url
       t.timestamps
     end
   end
