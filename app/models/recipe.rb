9f17f30f7c173211c5e2f29379c40d37e153abbc
Rémon Wobbema <remonwobbema@MacBook-Air-van-Remon.local>
2023-12-01 11:36:35 +0100

    added validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
   belongs_to :category
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  validates :title, :cook_time, :prep_time, :ratings, :image_url, presence: true
 end
